ed6de6312a5a3599f7a984ff67ae7b8a3f26d52c
Joachim Ringgaard <kwh131@alumni.ku.dk>
2022-01-28 12:08:23 +0100

    started collection-preview on shop-page

--- src/App.js
+++ src/App.js
@@ -2,30 +2,17 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
-import { Component } from 'react/cjs/react.production.min';
-
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-)
-
-const JacketsPage = () => (
-  <div>
-    <h1>JACKETS PAGE </h1>
-  </div>
-)
+import { Component } from 'react';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
-        <Route path='/shop/jackets' component={JacketsPage} />
+        <Route path='/shop' component={ShopPage} />
 
       </Switch>
     </div>
